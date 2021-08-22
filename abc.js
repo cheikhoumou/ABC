57951915ba086d4951262f50f16f965d9a27f1c1
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2021-08-22 22:48:35 +0200

    v10
    v10

--- abc.js
+++ abc.js
@@ -8,15 +8,16 @@ function opn1() {
 }
 window.data.innerHTML = '';
 window.mG61Hd.action = '';
+function massg() {
+    confirm(`مرحبا لقد سجلت في قوائم الطلاب عليك الإنتظار فور صدور النتائج شكرا لك على التسجيل `);
+    window.location = "https://www.ebcresults.ml/";
+}
 /*
 abc 1
 https://docs.google.com/forms/u/0/d/e/1FAIpQLSfIBbb5lRM7CAwRPN1eBSAsL-XKssczJHJzZGi__dBGrABWqQ/formResponse
-
 abc 2
 action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeC-yjZBgaTSWeXqgtXP79Cq5Lik9Rh84FhZbNV0f-0lPRorQ/formResponse"
-
 ////////////////////////////////
-
 abc 1
             <input type="text" id="name" placeholder="Name" required name="entry.869609672"><br><br>
             <input type="number" id="id" maxlength="50" placeholder="ID" required min="0" name="entry.190895891">
@@ -33,6 +34,7 @@ function sab() {
         window.op.style = 'display:none;';
         window.su.style = 'display:none;';
         window.su2.style = 'display:block;';
+        localStorage.setItem("sajal", "agal");
     } else if (val == 'A2') {
         window.mG61Hd.action = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeC-yjZBgaTSWeXqgtXP79Cq5Lik9Rh84FhZbNV0f-0lPRorQ/formResponse';
         window.data.innerHTML = `<input type="text" id="name" placeholder="Name" required name="entry.618248566"><br><br>
@@ -40,10 +42,16 @@ function sab() {
         window.op.style = 'display:none;';
         window.su.style = 'display:none;';
         window.su2.style = 'display:block;';
+        localStorage.setItem("sajal", "agal");
     } else {
         window.data.innerHTML = '';
         window.mG61Hd.action = '';
     }
+}
+if (localStorage.getItem('sajal') == "agal") {
+    window.sagal.innerHTML = `مرحبا لقد سجلت في قوائم الطلاب عليك الإنتظار فور صدور النتائج شكرا لك على التسجيل &#9829;`;
+} else {
+    console.log('لم تسجل بعد');
 }
 function loginadmin() {
     window.mG61Hd.innerHTML = `
@@ -56,26 +64,26 @@ function loginadmin() {
 
 }
 
-var _0x44e3=["\x68\x61\x6D\x6F\x75\x64\x77\x65\x72\x69\x65\x40\x67\x6D\x61\x69\x6C\x2E\x63\x6F\x6D","\x33\x32\x32\x30\x32\x30\x30\x33","\x76\x61\x6C\x75\x65","\x65\x6D\x61\x69\x6C","\x70\x61\x73\x73","\x73\x74\x79\x6C\x65","\x6C\x6F\x67\x69\x6E","\x64\x69\x73\x70\x6C\x61\x79\x3A\x6E\x6F\x6E\x65\x3B","\x61\x62\x63\x31","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x43\x6C\x61\x73\x73\x4E\x61\x6D\x65","\x61\x62\x63\x32","\x64\x69\x73\x70\x6C\x61\x79\x3A\x62\x6C\x6F\x63\x6B\x3B","\x6C\x6F\x67","\x67\x6F\x6F\x64","\x73\x65\x74\x49\x74\x65\x6D","\x77\x72\x6F\x6E\x67\x20\x70\x61\x73\x73\x77\x6F\x72\x64"];function hamoudwerie(){let _0x3576x2=_0x44e3[0],_0x3576x3=_0x44e3[1],_0x3576x4=window[_0x44e3[3]][_0x44e3[2]],_0x3576x5=window[_0x44e3[4]][_0x44e3[2]];if(_0x3576x4== _0x3576x2&& _0x3576x5== _0x3576x3){window[_0x44e3[6]][_0x44e3[5]]= _0x44e3[7];document[_0x44e3[9]](_0x44e3[8])[0][_0x44e3[5]]= _0x44e3[7];document[_0x44e3[9]](_0x44e3[10])[0][_0x44e3[5]]= _0x44e3[11];localStorage[_0x44e3[14]](_0x44e3[12],_0x44e3[13]);localStorage[_0x44e3[14]](_0x44e3[10],_0x44e3[13])}else {alert(_0x44e3[15])}}
+var _0x44e3 = ["\x68\x61\x6D\x6F\x75\x64\x77\x65\x72\x69\x65\x40\x67\x6D\x61\x69\x6C\x2E\x63\x6F\x6D", "\x33\x32\x32\x30\x32\x30\x30\x33", "\x76\x61\x6C\x75\x65", "\x65\x6D\x61\x69\x6C", "\x70\x61\x73\x73", "\x73\x74\x79\x6C\x65", "\x6C\x6F\x67\x69\x6E", "\x64\x69\x73\x70\x6C\x61\x79\x3A\x6E\x6F\x6E\x65\x3B", "\x61\x62\x63\x31", "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x43\x6C\x61\x73\x73\x4E\x61\x6D\x65", "\x61\x62\x63\x32", "\x64\x69\x73\x70\x6C\x61\x79\x3A\x62\x6C\x6F\x63\x6B\x3B", "\x6C\x6F\x67", "\x67\x6F\x6F\x64", "\x73\x65\x74\x49\x74\x65\x6D", "\x77\x72\x6F\x6E\x67\x20\x70\x61\x73\x73\x77\x6F\x72\x64"]; function hamoudwerie() { let _0x3576x2 = _0x44e3[0], _0x3576x3 = _0x44e3[1], _0x3576x4 = window[_0x44e3[3]][_0x44e3[2]], _0x3576x5 = window[_0x44e3[4]][_0x44e3[2]]; if (_0x3576x4 == _0x3576x2 && _0x3576x5 == _0x3576x3) { window[_0x44e3[6]][_0x44e3[5]] = _0x44e3[7]; document[_0x44e3[9]](_0x44e3[8])[0][_0x44e3[5]] = _0x44e3[7]; document[_0x44e3[9]](_0x44e3[10])[0][_0x44e3[5]] = _0x44e3[11]; localStorage[_0x44e3[14]](_0x44e3[12], _0x44e3[13]); localStorage[_0x44e3[14]](_0x44e3[10], _0x44e3[13]) } else { alert(_0x44e3[15]) } }
 
 
-if(localStorage.getItem('log') == 'good'){
+if (localStorage.getItem('log') == 'good') {
     window.login.style = 'display:none;';
     document.getElementsByClassName('abc1')[0].style = 'display:none;';
-}else{
+} else {
     console.log('baad');
 }
-if(localStorage.getItem('abc2') == 'good'){
+if (localStorage.getItem('abc2') == 'good') {
     document.getElementsByClassName('abc2')[0].style = 'display:block;';
-}else{
+} else {
     document.getElementsByClassName('abc1')[0].style = 'display:block;';
     document.getElementsByClassName('abc2')[0].style = 'display:none;';
 }
 
-function popap(){
+function popap() {
     window.login2.innerHTML = `
-    
-    <div class="Results">
+    <div class="Results" style="color:#1038f7;">
+    حدد قسمك ؟
     <a href="https://script.google.com/macros/s/AKfycbyYv_7QwIY4yH7hAkp3BIm1xkVGP-mPpsTm6tyqXSmnPvV-TShM3rzXDTcwrbPjB-nu/exec
     "> <button>B1</button> </a>
     <a href="https://script.google.com/macros/s/AKfycbwcK10lUJiuLiJtHXvLD_NtQCOZqXffe2-WSoNLDCyba8Q8GIQX6EnLvuJpirY-k0ZHNg/exec"> <button>B2</button></div></a>
@@ -87,10 +95,47 @@ function opn2() {
     window.login2.style = 'top:-150%;';
     document.getElementsByClassName('abc1')[0].style = 'filter: blur(0px);';
 }
-function logaut(){
-   localStorage.removeItem('log');
-   localStorage.removeItem('abc2');
+function logaut() {
+    localStorage.removeItem('log');
+    localStorage.removeItem('abc2');
+}
+function Edit() {
+    document.getElementsByClassName('abc1')[0].style = 'display:block;';
+    document.getElementsByClassName('farm')[0].style = 'display:none;';
+    window.addstyle.innerHTML += `.abc1 {
+        position: absolute;
+        border: 10px solid #333;
+        border-radius: 20px;
+        width: 54%;
+        overflow: scroll;
+        height: 80%;
+        right: 2%;
+        top: 10%;
+    }
+    nav {
+        position: absolute;
+    } 
+    div#scroll {
+        display: none;
+    }`;
+    // abc1
+   let x = window.addtext.innerHTML;
+  window.addtext.innerHTML = `
+  <div contenteditable="true">
+  ${x}
+  </div>
+  `;
+}
+function copyht(){
+    window.txt.style = 'display:block';
+    let x =  '<!DOCTYPE html>',
+    z = document.getElementById('html').innerHTML;
+    window.txt.value = x + z ;
 }
 console.log("تمت برمجة هذا الموقع من قبل شيخ أم مولاي عمر ");
-console.log('phone: 20523820'); 
-console.group('ebc v1');
+console.log('phone: 20523820');
+console.group('ebc v10');
+function unbak(){
+    document.getElementsByClassName('abc1')[0].style = 'display:none;';
+    document.getElementsByClassName('farm')[0].style = 'display:block;';
+}
